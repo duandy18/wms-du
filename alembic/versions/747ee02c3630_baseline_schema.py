e460b43fdd19c2888e17e46333b308a778e77e34
hongshi <hongshi@users.noreply.github.com>
2025-09-29 04:44:56 +0800

    chore(repo): clean db artifacts; WIP snapshot before hooks

---
+++ alembic/versions/747ee02c3630_baseline_schema.py
@@ -0,0 +1,21 @@
+"""baseline schema
+
+Revision ID: 747ee02c3630
+Revises: 
+Create Date: 2025-09-29 03:27:41.794991
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '747ee02c3630'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
